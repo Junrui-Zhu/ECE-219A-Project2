433a0d6aff04afdcefa06b841b685cb4a5218d8c
Andrew Xing <andrewxing43@g.ucla.edu>
2025-02-03 12:53:42 -0800

    modified all_tfidf.py to accept any types of categories

--- nndl/all_tfidf_labels.py
+++ nndl/all_tfidf_labels.py
@@ -5,9 +5,8 @@ Also, by running this directly, you get answer for question 1
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def get_tfidf_labels(class1 = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware']
-    , class2 = ['rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']):
-    newsgroups_all = fetch_20newsgroups(subset='all', remove = ('headers', 'footers', 'quotes'), categories = class1 + class2)
+def get_tfidf_labels(category = None):
+    newsgroups_all = fetch_20newsgroups(subset='all', remove = ('headers', 'footers', 'quotes'), categories = category)
     
     vectorizer = TfidfVectorizer(min_df = 3, stop_words = 'english')
     all_tfidf = vectorizer.fit_transform(newsgroups_all.data)
@@ -21,5 +20,5 @@ def get_tfidf_labels(class1 = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp
 if __name__ == "__main__":
     class1 = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware']
     class2 = ['rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
-    all_tfidf, _ = get_tfidf_labels(class1, class2)
+    all_tfidf, _ = get_tfidf_labels(class1 + class2)
     print(all_tfidf.shape)
