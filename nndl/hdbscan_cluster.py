58ed1d9cbe037a23a4f76f276e1a254279cede6a
BuildTools <unconfigured@null.spigotmc.org>
2025-02-04 03:35:50 -0800

    finished q15 and q16

--- nndl/hdbscan_cluster.py
+++ nndl/hdbscan_cluster.py
@@ -1,5 +1,8 @@
 import hdbscan
+from all_tfidf_labels import get_tfidf_labels
+from k_mean import cluster_measures, k_mean_evaluate
 from scipy.spatial.distance import pdist, squareform
+from umap_reduction import umap_dim_reduce
 
 def hdbscan_clustering(X, min_cluster_size=10, min_samples=5, metric='euclidean'):
     if metric != 'cosine':
@@ -10,3 +13,15 @@ def hdbscan_clustering(X, min_cluster_size=10, min_samples=5, metric='euclidean'
         distance_matrix = squareform(pdist(X, metric="cosine"))
         y_pred = clustering.fit_predict(distance_matrix)
     return y_pred, clustering
+
+if __name__ == "__main__":
+    X, y = get_tfidf_labels()
+    _, X = umap_dim_reduce(X, 20, "cosine")
+    min_cluster_size_list = [20, 100, 200]
+    for min_cluster_size in min_cluster_size_list:
+        y_pred, _ = hdbscan_clustering(X, min_cluster_size, metric='cosine')
+        measurement = cluster_measures(y, y_pred, f'min cluster size = {min_cluster_size}')
+
+    # min_cluster_size = 200 is the best
+    y_pred, _ = hdbscan_clustering(X, 200, metric='cosine')
+    k_mean_evaluate(y, y_pred, f' for min cluster size = 200')
