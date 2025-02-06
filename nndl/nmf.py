34ce2a801990188be85cbec91e86e521941958c5
JunRui Zhu <zhujr24@g.ucla.edu>
2025-02-05 16:34:37 -0800

    Add files via upload
    now is nmf

---
+++ nndl/nmf.py
@@ -0,0 +1,7 @@
+from sklearn.decomposition import NMF
+import numpy as np
+
+def nmf_dim_reduction(tfidf_matrix, n_components=10, random_state=42):
+    nmf_model = NMF(n_components=n_components, random_state=random_state, max_iter=500, init='nndsvd')# set max_iter = 500 to ensure convergence
+    W = nmf_model.fit_transform(tfidf_matrix)
+    return nmf_model, W
